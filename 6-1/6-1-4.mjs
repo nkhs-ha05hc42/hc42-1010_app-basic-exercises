e6b1677a2488587cd7f50c9d78ab67f0bd69bc48
makino-126 <nkhshc421010@g.neec.ac.jp>
2024-12-23 11:35:13 +0900

    6-1

---
+++ 6-1/6-1-4.mjs
@@ -0,0 +1,24 @@
+let a1 = false
+let b1 = false
+console.log(a1 || b1)
+console.log(a1 && b1)
+console.log(!a1)
+console.log(!b1)
+a1 = true
+b1 = true
+console.log(a1 || b1)
+console.log(a1 && b1)
+console.log(!a1)
+console.log(!b1)
+a1 = true
+b1 = false
+console.log(a1 || b1)
+console.log(a1 && b1)
+console.log(!a1)
+console.log(!b1)
+a1 = false
+b1 = true
+console.log(a1 || b1)
+console.log(a1 && b1)
+console.log(!a1)
+console.log(!b1)
