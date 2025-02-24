38ca55461e15b4e4ae775fc9ccadd79e1b49d239
makino-126 <nkhshc421010@g.neec.ac.jp>
2025-02-24 20:35:20 +0900

    9-8

---
+++ client/9-8/9-8-1.mjs
@@ -0,0 +1,47 @@
+const viewRecords = [
+    {
+    id: 1,
+    name: "山田",
+    age: 23,
+    postcode: "0620055",
+    address: "北海道札幌市",
+    average_score: 81.5,
+    },
+    {
+    id: 2,
+    name: "田中",
+    age: 18,
+    postcode: "0590465",
+    address: "北海道登別市",
+    average_score: 71,
+    },
+    ]
+    /**
+    * ウィンドウ読み込み時の処理
+    */
+    window.onload = () => {
+    const KadaiBody = document.getElementById("Kadaibody")
+    KadaiBody.innerHTML = ""
+    for (const record of viewRecords) {
+    const tr = document.createElement("tr")
+    KadaiBody.appendChild(tr)
+    const idTd = document.createElement("td")
+    idTd.appendChild(document.createTextNode(record.id))
+    tr.appendChild(idTd)
+    const nameTd = document.createElement("td")
+    nameTd.appendChild(document.createTextNode(record.name))
+    tr.appendChild(nameTd)
+    const ageTd = document.createElement("td")
+    ageTd.appendChild(document.createTextNode(record.age))
+    tr.appendChild(ageTd)
+    const postcodeTd = document.createElement("td")
+    postcodeTd.appendChild(document.createTextNode(record.postcode))
+    tr.appendChild(postcodeTd)
+    const addressTd = document.createElement("td")
+    addressTd.appendChild(document.createTextNode(record.address))
+    tr.appendChild(addressTd)
+    const average_scoreTd = document.createElement("td")
+    average_scoreTd.appendChild(document.createTextNode(record.average_score))
+    tr.appendChild(average_scoreTd)
+    }
+}
